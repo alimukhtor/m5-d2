f5010387fd39422f86850b09b5e7843fe3979aa9
alimukhtor <james1999007@gmail.com>
2021-11-30 16:06:48 +0100

    getting authors from endpoint

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import express from "express"
+
+import listEndpoints from "express-list-endpoints"
+
+import authorRouter from "./services/blogs/index.js"
+
+const server = express()
+
+const port = 3004
+
+server.use(express.json())
+
+
+// Endponts here
+
+
+server.use("/author", authorRouter)
+console.table(listEndpoints(server))
+
+
+
+server.listen(port, ()=> {
+    console.log(`Server is running on port ${port}`);
+})
--- src/services/blogs/index.js
+++ src/services/blogs/index.js
@@ -1,9 +1,51 @@
+import express from "express" 
+import fs from 'fs'
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
+import uniqid from "uniqid"
 
 const authorRouter = express.Router()
 
 
+const currentFilePath = fileURLToPath(import.meta.url) 
 
+console.log("Current Path is :", currentFilePath);
+
+const currentFolderPath = dirname(currentFilePath)
+console.log("Current folder is :", currentFolderPath);
+
+const authorJSONPath = join(currentFolderPath, "authors.json")
 
 authorRouter.post("/", (request, response)=> {
+   
+
+
+})
+authorRouter.get("/", (request, response)=> {
+    const readJsonFile = fs.readFileSync(authorJSONPath)
+    const authorArray = JSON.parse(readJsonFile)
+    console.log("FILE CONTENT: ", JSON.parse(readJsonFile))
+    response.send(authorArray)
+})
+
+
+
+
+authorRouter.get("/:authorId", (request, response)=> {
     
 })
+
+
+
+authorRouter.put("/:authorId", (request, response)=> {
+    
+})
+
+
+
+authorRouter.delete("/:authorId", (request, response)=> {
+    
+})
+
+
+export default authorRouter
