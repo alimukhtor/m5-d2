e8005d1280f416f23873640860ecd94216815789
alimukhtor <james1999007@gmail.com>
2021-12-01 17:02:01 +0100

    random

--- src/services/blogs/validation.js
+++ src/services/blogs/validation.js
@@ -2,13 +2,18 @@
 import { body } from "express-validator"
 
 export const authorValidation = [
-  body("category").exists().withMessage("Category is a mandatory field!"),
-  body("title").exists().withMessage("Title is a mandatory field!"),
-  body("cover").exists().withMessage("Cover is a mandatory field!"),
-  body("name").exists().withMessage("Name is a mandatory field!"),
-  body("readTime.value").exists().withMessage("ReadTime value is a mandatory field!"),
+  body("category").exists().withMessage("Category is a mandatory field!"), // frontend
+  body("title").exists().withMessage("Title is a mandatory field!"), // frontend
+  body("cover").exists().withMessage("Cover is a mandatory field!"), // frontend add
+  body("readTime.value").exists().withMessage("ReadTime value is a mandatory field!"), // calc at backend or send it from frontend
   body("readTime.unit").exists().withMessage("ReadTime unit is a mandatory field!"),
   body("author.name").exists().withMessage("Author name is a mandatory field!"),
   body("author.avatar").exists().withMessage("Author avatar is a mandatory field!"),
   body("content").exists().withMessage("Content filed is a mandatory field!"),
 ]
+
+/**
+ * 
+ * 
+ *   count all the words in content and multiply by average word read time 
+ */
